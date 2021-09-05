d458bc78377a7af0471cbe457773b2668289f00a
harshita kanwa <60289182+harshitakanwa@users.noreply.github.com>
2021-09-05 21:16:58 +0530

    Update tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -14,8 +14,63 @@ CREATE TABLE USER (
     UID BIGINT NOT NULL Unique check(100000000000 <= UID <= 999999999999)
     );
 
+
+
 # Login details
 CREATE TABLE LOGIN (
     username varchar(16) Unique PRIMARY KEY check(CHAR_LENGTH(username)>=8),
     password varchar(100) NOT NULL check(CHAR_LENGTH(password)>=8)
 );
+
+
+
+#User_Library_Data
+
+CREATE TABLE User_Library_Data(Username VARCHAR(100),
+            No_Of_Books INT(10),
+            Max_No_Of_Books INT(10),
+           Joined_On DATE,Library_ID VARCHAR(100) UNIQUE,
+           Photo MEDIUMBLOB NOT NULL,
+           Admin CHAR(100),
+           Fine CHAR(100),
+          primary key(Library_ID),
+       FOREIGN KEY(Username)REFERENCES Login_Details(Username) );
+
+
+
+#Subscription
+CREATE TABLE Subscription(Subscription_ID VARCHAR(100) PRIMARY KEY,
+         Subscription_Amount INT(10),
+        Max_Books INT(10));
+  
+  
+ 
+ 
+ #Book
+ create table Book(Book_ID VARCHAR(100) UNIQUE,
+              Title CHAR(100),
+              Auther CHAR(100),
+            Publication VARCHAR(100),
+            Edition VARCHAR(100),
+            No_OF_Copies INT(10),
+            Date_Of_Purchase DATE,
+             No_Of_Pages INT(100),
+            Prices INT(10),
+            Type CHAR(100),
+             Genere VARCHAR(100), 
+             Age_rating INT(5),
+             Current_Book_Holder CHAR(100), 
+             PRIMARY KEY(Book_ID));
+ 
+ 
+ 
+ 
+ #Fine_table
+  CREATE TABLE Fine_table(User_Library_ID VARCHAR(100),
+        Book_ID VARCHAR(100),
+        Fine_Amount INT(10),
+        Paid ENUM('Yes','No'),
+        Paid_Date DATE, 
+       FOREIGN KEY(User_Library_ID) REFERENCES User_Library_Data(Username)
+       FOREIGN KEY(Book_ID) REFERENCES Book(Book_ID));
+        
