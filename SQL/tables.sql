3c320431058bd0638d338ef6e08f26552e711dd1
Jithendra Sri SivaKesava Siddartha Puppala <71070388+jithendra1798@users.noreply.github.com>
2021-09-05 15:28:08 +0530

    Update tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -3,11 +3,12 @@ use LIBRARY;
 
 # User Registration Details
 CREATE TABLE USER (
+    library_id int NOT NULL PRIMARY KEY AUTO_INCREMENT,
     full_name varchar(50) NOT NULL,
     mobile BIGINT(10) check(1000000000<=mobile<=9999999999),
     email varchar(300) NOT NULL,
     DOB DATE NOT NULL,
-    gender char(1) NOT NULL check(gender="M" or gender="F),
+    gender char(1) NOT NULL check(gender="M" or gender="F"),
     joined_date DATE NOT NULL,
     username varchar(16) NOT NULL References LOGIN(username),
     UID BIGINT(12) NOT NULL Unique
