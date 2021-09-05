35cd5a07bc2179d1ab567a46328be928d9948df3
Jithendra Sri SivaKesava Siddartha Puppala <71070388+jithendra1798@users.noreply.github.com>
2021-09-05 15:11:50 +0530

    Update tables.sql

--- SQL/tables.sql
+++ SQL/tables.sql
@@ -1,3 +1,18 @@
 CREATE DATABASE LIBRARY;
-use database LIBRARY;
+use LIBRARY;
 
+# User Registration Details
+CREATE TABLE USER (
+    full_name varchar(50) NOT NULL,
+    mobile BIGINT(10) check(1000000000<=mobile<=9999999999),
+    email varchar(300) NOT NULL,
+    DOB DATE NOT NULL,
+    gender char(1) NOT NULL check(gender="M" or gender="F),
+    joined_date DATE NOT NULL,
+    username varchar(16) NOT NULL References ,
+    UID BIGINT(12) NOT NULL Unique
+);
+
+# Login details
+CREATE TABLE LOGIN (
+);
